603e42bed7cd4dbe00632df81587efa02e78cc83
eric <eric.ede@gmail.com>
2025-12-23 02:00:04 -0300

    tratamento para senha null

--- src/main/java/com/example/validasenha/controller/dto/ValidatorPasswordRequest.java
+++ src/main/java/com/example/validasenha/controller/dto/ValidatorPasswordRequest.java
@@ -1,8 +1,10 @@
 package com.example.validasenha.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ValidatorPasswordRequest(
-    @NotBlank(message = "Senha não pode ser nulla ou vazia")
+    @NotBlank(message = "Senha não pode ser vazia")
+    @NotNull(message = "Senha não pode ser nulla")
     String password) {
 }
